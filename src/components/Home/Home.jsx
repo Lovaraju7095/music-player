c9a5364c779c708651cb91a7607f1425faa5d3df
Lovaraju7095 <lovarajuruthala903@gmail.com>
2024-08-03 23:45:03 +0530

    adding react icons

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,6 +1,10 @@
-import React, { useEffect, useState, useRef, useMemo } from "react";
-import "./home.css";
+import React, { useEffect, useState, useRef, useMemo} from "react";
+import './home.css';
 import logo from "../../assets/spotify-logo.png";
+import profile from "../../assets/profile.png";
+import { FaPlay, FaPause, FaStepForward, FaStepBackward, FaVolumeMute, FaVolumeUp } from "react-icons/fa";
+
+
 
 const Home = () => {
   const [data, setData] = useState([]);
@@ -12,44 +16,45 @@ const Home = () => {
   const [volume, setVolume] = useState(1);
   const [progress, setProgress] = useState(0);
   const [isMuted, setIsMuted] = useState(false);
-  const [currentTab, setCurrentTab] = useState("You");
-  const [searchTerm, setSearchTerm] = useState("");
+  const [playbackRate, setPlaybackRate] = useState(1);
+  const [showMenu, setShowMenu] = useState(false);
+  const [searchTerm, setSearchTerm] = useState('');
   const audioRef = useRef(null);
-  const [backgroundColor, setBackgroundColor] = useState("#071952");
+  const [backgroundColor, setBackgroundColor] = useState('#071952');
+  const [currentTab, setCurrentTab] = useState("You");
+
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await fetch(
-          "https://cms.samespace.com/items/songs"
-        );
-        if (!response.ok) {
-          throw new Error("Failed to fetch data");
-        }
+        const response = await fetch("https://cms.samespace.com/items/songs");
         const result = await response.json();
-        const updatedData = result.data.map((eachItem) => ({
-          accent: eachItem.accent,
-          artist: eachItem.artist,
-          cover: eachItem.cover,
-          dateCreated: new Intl.DateTimeFormat("en-US", {
-            hour: "2-digit",
-            minute: "2-digit",
-            hour12: false,
-          }).format(new Date(eachItem.date_created)),
-          id: eachItem.id,
-          name: eachItem.name,
-          sort: eachItem.sort,
-          status: eachItem.status,
-          topTrack: eachItem.top_track,
-          url: eachItem.url,
-          userCreated: eachItem.user_created,
-          userUpdated: eachItem.user_updated,
-          thumbnail: `https://cms.samespace.com/assets/${eachItem.cover}`,
-        }));
-        setData(updatedData);
-        setLoading(false);
+
+        if (response.ok) {
+          const resultData = result.data;
+          const updatedData = resultData.map(eachItem => ({
+            accent: eachItem.accent,
+            artist: eachItem.artist,
+            cover: eachItem.cover,
+            dateCreated: new Intl.DateTimeFormat('en-US', { hour: '2-digit', minute: '2-digit', hour12: false }).format(new Date(eachItem.date_created)),
+            id: eachItem.id,
+            name: eachItem.name,
+            sort: eachItem.sort,
+            status: eachItem.status,
+            topTrack: eachItem.top_track,
+            url: eachItem.url,
+            userCreated: eachItem.user_created,
+            userUpdated: eachItem.user_updated,
+            thumbnail: `https://cms.samespace.com/assets/${eachItem.cover}`
+          }));
+          setData(updatedData);
+          // setSelectedSong(updatedData[0])
+        } else {
+          throw new Error('Failed to fetch data');
+        }
       } catch (error) {
         setError(error.message);
+      } finally {
         setLoading(false);
       }
     };
@@ -75,16 +80,56 @@ const Home = () => {
     }
   };
 
-  const handleNextPreviousSong = (direction) => {
+  const handleNextSong = () => {
     if (currentSongIndex !== null) {
-      let newIndex =
-        direction === "next"
-          ? (currentSongIndex + 1) % data.length
-          : (currentSongIndex - 1 + data.length) % data.length;
-      setSelectedSong(data[newIndex]);
-      setCurrentSongIndex(newIndex);
-      setBackgroundColor(data[newIndex].accent || "#fff");
+      const nextIndex = (currentSongIndex + 1) % data.length;
+      setSelectedSong(data[nextIndex]);
+      setCurrentSongIndex(nextIndex);
+
+      setBackgroundColor(data[nextIndex].accent || '#fff');
     }
+  };
+
+  const handlePreviousSong = () => {
+    if (currentSongIndex !== null) {
+      const prevIndex = (currentSongIndex - 1 + data.length) % data.length;
+      setSelectedSong(data[prevIndex]);
+      setCurrentSongIndex(prevIndex);
+
+      setBackgroundColor(data[prevIndex].accent || '#fff');
+    }
+  };
+
+ 
+  const currentSongsList = useMemo(() => {
+    let filteredSongs;
+    if (currentTab === "You") {
+      filteredSongs= data;
+    } else {
+      filteredSongs= data.filter((item) => item.topTrack);
+    }
+    if(searchTerm){
+      return filteredSongs.filter(song =>
+        song.name.toLowerCase().includes(searchTerm.toLowerCase()) ||
+        song.artist.toLowerCase().includes(searchTerm.toLowerCase())
+      );
+      return filteredSongs
+    }
+    
+    return filteredSongs
+  }, [data, currentTab,searchTerm]);
+  
+  function onShowSong(songId) {
+    const songIndex = currentSongsList.findIndex(song => song.id === songId);
+    const song = currentSongsList[songIndex];
+    setSelectedSong(song);
+    setCurrentSongIndex(songIndex);
+
+    setBackgroundColor(song.accent || '#fff');
+  }
+
+  const onChangeTopTab = (tab) => {
+    setCurrentTab(tab);
   };
 
   const handleMuteUnmute = () => {
@@ -107,26 +152,16 @@ const Home = () => {
 
   const handleAudioTimeUpdate = () => {
     if (audioRef.current) {
-      setProgress(
-        (audioRef.current.currentTime / audioRef.current.duration) * 100
-      );
+      setProgress((audioRef.current.currentTime / audioRef.current.duration) * 100);
     }
   };
 
-  const currentSongsList = useMemo(() => {
-    let filteredSongs = data;
-    if (currentTab !== "You") {
-      filteredSongs = data.filter((item) => item.topTrack);
-    }
-    if (searchTerm) {
-      filteredSongs = filteredSongs.filter(
-        (song) =>
-          song.name.toLowerCase().includes(searchTerm.toLowerCase()) ||
-          song.artist.toLowerCase().includes(searchTerm.toLowerCase())
-      );
-    }
-    return filteredSongs;
-  }, [data, currentTab, searchTerm]);
+
+
+  
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error: {error}</p>;
 
   return (
     <div className="container-fluid" id="ChangeBgColor" style={{ backgroundColor }}>
@@ -134,31 +169,31 @@ const Home = () => {
         <div className="col-12 col-lg-2 start-card" style={{ height: '100vh' }}>
           <img src={logo} alt="logo image" className="logo" />
           <div className="avatar">
-            {/* Avatar content */}
+            <img src ={profile} className="avatar"/>
           </div>
         </div>
 
         <div className="col-12 col-lg-4 p-0">
           <div className="top-track-section">
-            <button className="top-track-tit1" style={currentTab === "You" ? { color: "#fff" } : { color: "grey" }} onClick={() => setCurrentTab("You")}>
+          <button className="top-track-tit1" style={currentTab=="You"? {color:'#fff'}:{color:'grey'}} onClick={() => onChangeTopTab("You")}>
               For You
             </button>
-            <button className="top-track-tit2" style={currentTab === "Tracks" ? { color: "#fff" } : { color: "grey" }} onClick={() => setCurrentTab("Tracks")}>
+            <button className="top-track-tit2" onClick={() => onChangeTopTab("Tracks")}  style={currentTab=="Tracks"? {color:'#fff'}:{color:'grey'}}>
               Top Tracks
             </button>
           </div>
-          <input
-            type="search"
-            placeholder="Search by song or artist"
-            value={searchTerm}
-            onChange={(e) => setSearchTerm(e.target.value)}
-            className="search-inp"
-          />
-          {currentSongsList.length > 0 && (
+            <input
+              type="search"
+              placeholder="Search by song or artist"
+              value={searchTerm}
+              onChange={(e) => setSearchTerm(e.target.value)}
+              className="search-inp"
+            />
+          {currentSongsList?.length > 0 && (
             <div>
-              {currentSongsList.map((song) => (
-                <div key={song.id} className="songs-card" style={{ background: song.id === selectedSong?.id ? "#241e16" : "transparent" }}>
-                  <div className="songs-card1" onClick={() => setSelectedSong(song)}>
+              {currentSongsList?.map(song => (
+                <div key={song.id} className="songs-card" style={{background:(song.id==selectedSong?.id)?"rgba(0,0,0,0.2)":'transparent'}}>
+                  <div className="songs-card1" onClick={() => onShowSong(song.id)}>
                     <div className="inside-card">
                       <img src={song.thumbnail} alt={song.name} className="song-image" />
                       <div className="songs-desc">
@@ -177,37 +212,56 @@ const Home = () => {
         <div className="col-12 col-lg-6 active-song-main">
           <div className="active-song-card">
             <div className="active-card1">
-              <span className="card-tit1">{selectedSong ? selectedSong.name : "Select a song"}</span>
-              <span className="card-tit2">{selectedSong ? selectedSong.artist : ""}</span>
+              <span className="card-tit1">{selectedSong ? selectedSong.name : 'Select a song'}</span>
+              <span className="card-tit2">{selectedSong ? selectedSong.artist : ''}</span>
             </div>
-            <div className="active-card2">{selectedSong && <img src={selectedSong.thumbnail} alt={selectedSong.name} className="active-image" />}</div>
+            <div className="active-card2">
+              {selectedSong && <img src={selectedSong.thumbnail} alt={selectedSong.name} className="active-image" />}
+            </div>
             <div className="audio-player-card">
               {selectedSong && (
                 <div className="audio-player">
-                  <audio ref={audioRef} src={selectedSong.url} onTimeUpdate={handleAudioTimeUpdate} preload="metadata">
+                  <audio
+                    ref={audioRef}
+                    src={selectedSong.url}
+                    onTimeUpdate={handleAudioTimeUpdate}
+                    preload="metadata"
+                  >
                     Your browser does not support the audio element.
                   </audio>
                   <div className="audio-controls">
-                    <input type="range" min="0" max="100" value={progress} onChange={handleProgressChange} className="progress-bar" />
+                    <input
+                      type="range"
+                      min="0"
+                      max="100"
+                      value={progress}
+                      onChange={handleProgressChange}
+                      className="progress-bar"
+                    />
                     <div className="songs-btn-section">
-                      <button className="aud-btns" >
+                      <button className="aud-btns three-dots" onClick={() => setShowMenu(!showMenu)}>
                         ⋯
                       </button>
                       <div className="songs-play-card">
-                        <button className="control-btns" onClick={() => handleNextPreviousSong("previous")}>
-                          ◄◄
-                        </button>
+                        <button className="control-btns" onClick={handlePreviousSong}>    <FaStepBackward /></button>
                         <button className="control-btns" onClick={handlePlayPause}>
-                          {isPlaying ? "🔊" : "🔇"}
+                        {isPlaying ? <FaPause /> : <FaPlay />}
                         </button>
-                        <button className="control-btns" onClick={() => handleNextPreviousSong("next")}>
-                          ►►
-                        </button>
+                        <button className="control-btns" onClick={handleNextSong}>   <FaStepForward /></button>
                       </div>
                       <button className="aud-btns" onClick={handleMuteUnmute}>
-                        {isMuted ? "🔇" : "🔊"}
+                      {isMuted ? <FaVolumeMute /> : <FaVolumeUp />}
                       </button>
                     </div>
+                    {/* {showMenu && (
+                      <div className="menu">
+                        <button onClick={() => handlePlaybackRateChange(0.5)}>0.5x</button>
+                        <button onClick={() => handlePlaybackRateChange(1)}>1x</button>
+                        <button onClick={() => handlePlaybackRateChange(1.5)}>1.5x</button>
+                        <button onClick={() => handlePlaybackRateChange(2)}>2x</button>
+                        <button onClick={handleDownload}>Download</button>
+                      </div>
+                    )} */}
                   </div>
                 </div>
               )}
